bf8d93f9c630e3a2f281137902a83e4eb88e72c6
Akin Oyedele <fibonacci@fibonnaci.local>
2025-08-14 06:50:22 -0400

    posthog config updates

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
-import * as Sentry from '@sentry/react';
+import * as Sentry from '@sentry/react'
 import { PostHogProvider } from 'posthog-js/react'
 
 const options = {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
   defaults: '2025-05-24',
+  capture_exceptions: true, // Enables capturing exceptions using Error Tracking
+  debug: import.meta.env.MODE === 'development',
 }
 
 Sentry.init({
@@ -21,18 +23,19 @@ Sentry.init({
   tracesSampleRate: 1.0,
   replaysSessionSampleRate: 0.1, // This records 10% of sessions.
   replaysOnErrorSampleRate: 1.0, // This records 100% of sessions that have an error.
-  sendDefaultPii: true
-});
+  sendDefaultPii: true,
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-        <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
-    <BrowserRouter>
-      <AuthProvider>
-        <Sentry.ErrorBoundary fallback={<div>An error has occurred. Please refresh the page.</div>}>
-          <App />
-        </Sentry.ErrorBoundary>
-      </AuthProvider>
-    </BrowserRouter>
+    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
+      <BrowserRouter>
+        <AuthProvider>
+          <Sentry.ErrorBoundary fallback={<div>An error has occurred. Please refresh the page.</div>}>
+            <App />
+          </Sentry.ErrorBoundary>
+        </AuthProvider>
+      </BrowserRouter>
+    </PostHogProvider>
   </StrictMode>,
 )
