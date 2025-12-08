adf84712b2e3c4cd82971ea2ab70b70401a5deab
Akin Oyedele <fibonacci@fibonnaci.local>
2025-12-08 07:00:09 -0500

    refactor: Move demo recording test to dedicated tests/demo directory

--- tests/demo/demo-recording.spec.ts
+++ tests/demo/demo-recording.spec.ts
@@ -1,5 +1,5 @@
 import { test } from '@playwright/test';
-import { programmaticLogin, navigateToRoute } from './helpers';
+import { programmaticLogin, navigateToRoute } from '../e2e/helpers';
 
 /**
  * Demo Recording Test
